1821f9afb51df839ae85deb4f7a65679d6ec3dc8
Olli Wang <olliwang@ollix.com>
2013-11-19 21:32:04 +0800

    Initializes the setup.py file and project structure.

---
+++ makegyp/bin/mkgyp.py
@@ -0,0 +1,3 @@
+#!/usr/bin/env python
+if __name__ == '__main__':
+    from makegyp.core import command
---
+++ makegyp/core/command.py
@@ -0,0 +1,8 @@
+import argparse
+
+
+parser = argparse.ArgumentParser(
+    description='Generates existing C or C++ library with GYP support.'
+)
+
+args = parser.parse_args()
