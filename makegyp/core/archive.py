b2e79be8254da4d7362fa5b0d4cf3faadee6f102
Olli Wang <olliwang@ollix.com>
2013-11-21 17:52:16 +0800

    Allows to choose the directory for containing extracted archives.

--- makegyp/core/archive.py
+++ makegyp/core/archive.py
@@ -6,16 +6,19 @@ tarfile_exts = ('.tar', '.tar.gz')
 all_archive_exts = (tarfile_exts,)
 
 
-def extract_archive(path):
-    ext = splitext(path)[1]
+def extract_archive(src, dest=None):
+    if dest is None:
+        dest = os.path.dirname(src)
+
+    ext = splitext(src)[1]
     if ext in tarfile_exts:
         # Makes sure the file is a valid tar archive.
-        if not tarfile.is_tarfile(path):
-            print 'Invalid Tar archive: %s' % path
+        if not tarfile.is_tarfile(src):
+            print 'Invalid Tar archive: %s' % src
             exit(1)
 
-        archive = tarfile.open(path)
-        archive.extractall(path=os.path.dirname(path))
+        archive = tarfile.open(src)
+        archive.extractall(path=dest)
 
 def splitext(name):
     for archive_exts in all_archive_exts:
