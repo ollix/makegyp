b523eb99d3c06c537ae17b9952870e88b7cc1c51
Olli Wang <olliwang@ollix.com>
2013-11-24 12:56:38 +0800

    Supports zip format for archives.

--- makegyp/core/archive.py
+++ makegyp/core/archive.py
@@ -1,9 +1,11 @@
 import os
 import tarfile
+import zipfile
 
 
 tarfile_exts = ('.tar', '.tar.gz')
-all_archive_exts = (tarfile_exts,)
+zip_exts = ('.zip',)
+all_archive_exts = (tarfile_exts, zip_exts)
 
 
 def extract_archive(src, dest=None):
@@ -19,6 +21,17 @@ def extract_archive(src, dest=None):
 
         archive = tarfile.open(src)
         archive.extractall(path=dest)
+    elif ext in zip_exts:
+        if not zipfile.is_zipfile(src):
+            print 'Invalid Zip archive: %s' % src
+            exit(1)
+
+        archive = zipfile.ZipFile(src)
+        archive.extractall(path=dest)
+        archive.close()
+    else:
+        print "Archive format is not supported: %r" % ext
+        exit(1)
 
 def splitext(name):
     for archive_exts in all_archive_exts:
