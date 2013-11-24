b0b568928aad14b388b89edd88747970e5936e02
Olli Wang <olliwang@ollix.com>
2013-11-24 17:12:38 +0800

    Adds google test formula and supports CMake build.

---
+++ makegyp/formula/gtest.py
@@ -0,0 +1,14 @@
+from makegyp import formula
+from makegyp.core import parser
+
+
+class Gtest(formula.Formula):
+    parser = parser.CmakeParser()
+    url = 'https://googletest.googlecode.com/files/gtest-1.7.0.zip'
+    sha256 = '247ca18dd83f53deb1328be17e4b1be31514cedfc1e3424f672bf11fd7e0d60d'
+
+    def configure(self):
+        return ['cmake', '.', '-G', 'Unix Makefiles', '-DBUILD_SHARED_LIBS=OFF']
+
+    def make(self):
+        return ['make']
