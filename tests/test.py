b832c13b2a119b32f26d34da59eff001e884ba28
Olli Wang <olliwang@ollix.com>
2013-12-08 16:02:17 +0800

    Fixes the test mechanism and shows test results.

--- tests/test.py
+++ tests/test.py
@@ -6,13 +6,17 @@ import sys
 
 
 module_path = os.path.abspath(__file__)
-test_dir = os.path.dirname(module_path)
+test_root_dir = os.path.dirname(module_path)
 
 def test_library(name):
-    print 'Test %r...' % name
+    print '* Test %r...' % name
     # Determines the directory of the tested library:
     test_dir = os.path.join(os.path.dirname(module_path), library_name)
     test_dir = os.path.abspath(test_dir)
+
+    if not os.path.isdir(test_dir):
+        print '!! No test found for %r' % name
+        return False
 
     # Switches directory:
     os.chdir(test_dir)
@@ -39,6 +43,7 @@ def test_library(name):
     executable_command = 'out/Debug/test'
     print '* Run %r' % executable_command
     subprocess.call(executable_command, shell=True)
+    return True
 
 
 if __name__ == "__main__":
@@ -54,11 +59,20 @@ if __name__ == "__main__":
     library_names = set(args.library_names)
     if not library_names:
         library_names = set()
-        for library_name in os.listdir(test_dir):
-            path = os.path.join(test_dir, library_name)
+        for library_name in os.listdir(test_root_dir):
+            path = os.path.join(test_root_dir, library_name)
             if os.path.isdir(path):
                 library_names.add(library_name)
 
+    successful_tests = set()
+    failed_tests = set()
     for library_name in library_names:
-        test_library(library_name)
+        if test_library(library_name):
+            successful_tests.add(library_name)
+        else:
+            failed_tests.add(library_name)
         print '=' * 3
+
+    print '* Test results:'
+    print '- Successful tests: %s' % sorted(successful_tests)
+    print '- Failed tests: %s' % sorted(failed_tests)
