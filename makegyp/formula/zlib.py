65f30a7dcb2fa5b497820ee273f315a71b8ff30e
Olli Wang <olliwang@ollix.com>
2013-12-09 15:37:04 +0800

    Fixes the formula for zlib.

--- makegyp/formula/zlib.py
+++ makegyp/formula/zlib.py
@@ -8,7 +8,7 @@ class Zlib(formula.Formula):
     sha256 = '36658cb768a54c1d4dec43c3116c27ed893e88b02ecfcb44f2166f9c0b7f2a0d'
 
     def configure(self):
-        return ['./configure', '--static']
+        return './configure --static'
 
     def make(self):
-        return ['make']
+        return 'make'
