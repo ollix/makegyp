7ccaa729bf96875941f192fa6a76d5727f3d7097
Olli Wang <olliwang@ollix.com>
2014-01-27 13:47:52 +0800

    Support for extracting .tar.xz files.
    Since the current makegyp adopt Python 2.7. It requires an additional pyliblzma library to support xz files. See http://goo.gl/tPxfTR for more information.
    
    Signed-off-by: Olli Wang <olliwang@ollix.com>

--- makegyp/core/archive.py
+++ makegyp/core/archive.py
@@ -1,11 +1,15 @@
+import contextlib
 import os
 import tarfile
 import zipfile
 
+import lzma
 
+
+lzma_exts = ('.tar.xz',)
 tarfile_exts = ('.tar', '.tar.bz2', '.tar.gz')
 zip_exts = ('.zip',)
-all_archive_exts = (tarfile_exts, zip_exts)
+all_archive_exts = (lzma_exts, tarfile_exts, zip_exts)
 
 
 def extract_archive(src, dest=None):
@@ -13,19 +17,21 @@ def extract_archive(src, dest=None):
         dest = os.path.dirname(src)
 
     ext = splitext(src)[1]
-    if ext in tarfile_exts:
+    if ext in lzma_exts:
+        with contextlib.closing(lzma.LZMAFile(src)) as xz:
+            with tarfile.open(fileobj=xz) as archive:
+                archive.extractall(path=dest)
+    elif ext in tarfile_exts:
         # Makes sure the file is a valid tar archive.
         if not tarfile.is_tarfile(src):
             print 'Invalid Tar archive: %s' % src
             exit(1)
-
         archive = tarfile.open(src)
         archive.extractall(path=dest)
     elif ext in zip_exts:
         if not zipfile.is_zipfile(src):
             print 'Invalid Zip archive: %s' % src
             exit(1)
-
         archive = zipfile.ZipFile(src)
         archive.extractall(path=dest)
         archive.close()
